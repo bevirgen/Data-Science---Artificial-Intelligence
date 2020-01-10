733bd87b9180baf856a44b27e6fb979907d4f19a
bevirgen <57602699+bevirgen@users.noreply.github.com>
2020-01-10 23:46:54 +0300

    Update NLP.py

--- 3- Machine Learning/2. DATAI Team/3. Natural Language Processing/NLP.py
+++ 3- Machine Learning/2. DATAI Team/3. Natural Language Processing/NLP.py
@@ -6,7 +6,7 @@ Created on Thu Jan  9 14:14:22 2020
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import numpy as np
@@ -43,22 +43,24 @@ for description in data.description:
     description = [lemma.lemmatize(word) for word in description]
     description = ' '.join(description)
     description_list.append(description)
-
+"""
 df = pd.DataFrame({'description':description_list})
 data = pd.concat([df.description, data.gender], axis=1)
+data.dropna(axis=0, inplace=True)
+"""
 # %%
-
-max_features = 50 #the most used 50 word
+max_features = 500 #the most used 50 word
 count_vectorizer = CountVectorizer(max_features=max_features,stop_words='english')
 sparce_matrix = count_vectorizer.fit_transform(description_list).toarray()
 print('The most used 50 word :',count_vectorizer.get_feature_names())
 # %% creating model
-y = data.iloc[:,1].values
+y = data.iloc[:,0].values
 x = sparce_matrix
 
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.1, random_state=42)
-
-# %% finding k value
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(x_train,y_train)
-y_pred = knn.predict(x_test)
+#%%
+nb = GaussianNB()
+nb.fit(x_train,y_train)
+#%%
+pred = nb.predict(x_test)
+print('accuracy:',nb.score(pred.reshape(-1,1),y_test))
